b7b688a1cfb5cfa950482ee3bacc90728cf2bc54
juanpchica <juanpchica@hotmail.com>
2021-04-24 15:59:16 -0500

    Class ui and event listeners created

---
+++ js/script.js
@@ -0,0 +1,42 @@
+class UI {
+  constructor() {
+    this.budgetFeedback = document.querySelector(".budget-feedback");
+    this.expenseFeedback = document.querySelector(".expense-feedback");
+    this.budgetForm = document.getElementById("budget-form");
+    this.budgetInput = document.getElementById("budget-input");
+    this.budgetAmount = document.getElementById("budget-amount");
+    this.expenseAmount = document.getElementById("expense-amount");
+    this.balance = document.getElementById("balance");
+    this.balanceAmount = document.getElementById("balance-amount");
+    this.expenseForm = document.getElementById("expense-form");
+    this.expenseInput = document.getElementById("expense-input");
+    this.amountInput = document.getElementById("amount-input");
+    this.expenseList = document.getElementById("expense-list");
+    this.itemList = [];
+    this.itemID = 0;
+  }
+}
+
+function eventListeners() {
+  const budgetForm = document.getElementById("budget-form");
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+
+  //Instance of UI Class
+  const ui = new UI();
+  console.log(ui);
+
+  budgetForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+  });
+
+  expenseForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+  });
+
+  expenseList.addEventListener("click", function () {});
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  eventListeners();
+});
